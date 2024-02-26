ebd26c34fdf6266e7070e5942f116507dde15167
Keyjekk <108344123+Keyjekk@users.noreply.github.com>
2024-02-26 09:53:11 +0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,109 @@
+<!DOCTYPE html>
+<html lang="ru">
+<head>
+  <meta charset="UTF-8">
+  <title>MALINKA bot</title>
+  <link href="styles/index.css" rel="stylesheet">
+</head>
+<body>
+    <div class="header_box">
+        <div class="header">
+            <div class="search">
+                <div class="search_box">
+                    <img src="img/search.svg">
+                    <input placeholder="Давайте искать..." class="search_field">
+                </div>
+                <div class="filters">
+                    <img src="img/filter.svg">
+                </div>
+            </div>
+            <div class="categories">
+                <div class="texts">
+                    <p class="name">Категории</p>
+                    <p class="category">Все товары</p>
+                </div>
+                <img src="img/forward.svg" alt="">
+            </div>
+        </div>
+    </div>
+    <div class="content">
+            <div class="card_box">
+                <div class="card">
+                    <div class="card_image">
+                        <img src="img/image.svg" alt="">
+                    </div>
+                    <div class="card_information">
+                        <div class="price_box">
+                            <p class="price">24235</p>
+                            <p class="vault">руб.</p>
+                        </div>
+                        <p class="name">Интерьер</p>
+                    </div>
+                    <button class="to_cart">
+                        <p class="add_cart">добавить</p>
+                    </button>
+                </div>
+                <div class="card">
+                    <div class="card_image">
+                        <img src="img/image.svg" alt="">
+                    </div>
+                    <div class="card_information">
+                        <div class="price_box">
+                            <p class="price">24235</p>
+                            <p class="vault">руб.</p>
+                        </div>
+                        <p class="name">Интерьер</p>
+                    </div>
+                    <button class="to_cart">
+                        <p class="add_cart">добавить</p>
+                    </button>
+                </div>
+                <div class="card">
+                    <div class="card_image">
+                        <img src="img/image.svg" alt="">
+                    </div>
+                    <div class="card_information">
+                        <div class="price_box">
+                            <p class="price">24235</p>
+                            <p class="vault">руб.</p>
+                        </div>
+                        <p class="name">Интерьер</p>
+                    </div>
+                    <button class="to_cart">
+                        <p class="add_cart">добавить</p>
+                    </button>
+                </div>
+                <div class="card">
+                    <div class="card_image">
+                        <img src="img/image.svg" alt="">
+                    </div>
+                    <div class="card_information">
+                        <div class="price_box">
+                            <p class="price">24235</p>
+                            <p class="vault">руб.</p>
+                        </div>
+                        <p class="name">Интерьер</p>
+                    </div>
+                    <button class="to_cart">
+                        <p class="add_cart">добавить</p>
+                    </button>
+                </div>
+                <div class="card">
+                    <div class="card_image">
+                        <img src="img/image.svg" alt="">
+                    </div>
+                    <div class="card_information">
+                        <div class="price_box">
+                            <p class="price">24235</p>
+                            <p class="vault">руб.</p>
+                        </div>
+                        <p class="name">Интерьер</p>
+                    </div>
+                    <button class="to_cart">
+                        <p class="add_cart">добавить</p>
+                    </button>
+                </div>
+            </div>
+    </div>
+</body>
+</html>
