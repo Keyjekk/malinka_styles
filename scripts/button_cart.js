9167909bab79b1d66e875042f432491d2e41d9d8
Keyjekk <108344123+Keyjekk@users.noreply.github.com>
2024-03-06 10:10:46 +0300

    Add files via upload

--- scripts/button_cart.js
+++ scripts/button_cart.js
@@ -2,18 +2,22 @@ let tg = window.Telegram.WebApp;
 
 tg.expand();
 
-tg.MainButton.text = "Тестовая кнопка"; //изменяем текст кнопки 
-tg.MainButton.textColor = "#F55353"; //изменяем цвет текста кнопки
-tg.MainButton.color = "#143F6B"; //изменяем цвет бэкграунда кнопки
-tg.MainButton.setParams({"color": "#143F6B"}); //так изменяются все параметры 
+tg.MainButton.text = "НАЧАТЬ ОПЛАТУ"; //изменяем текст кнопки 
+tg.MainButton.textColor = "#FFFFFF"; //изменяем цвет текста кнопки
+tg.MainButton.color = "#005DB4"; //изменяем цвет бэкграунда кнопки
+tg.MainButton.setParams({"color": "#005DB4"}); //так изменяются все параметры 
 
 let btn = document.getElementById("btnED"); //получаем кнопку активировать/деактивировать
 
-btn.addEventListener('click', function(){ //вешаем событие на нажатие html-кнопки
+//btn.addEventListener('click', function(){ //вешаем событие на нажатие html-кнопки
 if (tg.MainButton.isVisible){ //если кнопка показана 
 	tg.MainButton.hide() //скрываем кнопку 
 }
 else{ //иначе
     tg.MainButton.show() //показываем 
 }
+//});
+
+Telegram.WebApp.onEvent('mainButtonClicked', function(){
+	tg.sendData("Бот и сайт связаны!"); 
 });
